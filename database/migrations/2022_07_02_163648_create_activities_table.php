e0e00ddd1e607fc703d67a066aad6404c9a64511
ShinyQ <kurniadiahmadwijaya@gmail.com>
2022-07-02 23:51:35 +0700

    feat(env, method, activity): Changing base env configuration for postgre and setup model, controller and migration for method and activitiy

---
+++ database/migrations/2022_07_02_163648_create_activities_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('month');
+            $table->date('start');
+            $table->date('end');
+            $table->string('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
