98dd3e11b9a7d78d85118ef9b9f520a7b43a13e4
ShinyQ <kurniadiahmadwijaya@gmail.com>
2022-07-02 22:51:11 +0700

    feat(assets, index): Adding base view and resources for Bootstrap and JQuery

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Learning Activities</title>
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+</head>
+<body>
+<h1>Hello, world!</h1>
+
+<script src="{{ asset('assets/js/bootstrap.bundle.js') }}"></script>
+<script src="{{ asset('assets/js/jquery-3.6.0.min.js') }}"></script>
+</body>
+</html>
